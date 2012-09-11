7c1f760e9d2d4bfdf3d06bd6a31cb7f707ce40c0
Nathaniel Tucker <me@ntucker.me>
2012-09-10 22:57:45 -0700

    Relax character requirements for usernames

--- account/forms.py
+++ account/forms.py
@@ -10,7 +10,7 @@ from account.conf import settings
 from account.models import EmailAddress
 
 
-alnum_re = re.compile(r"^\w+$")
+alnum_re = re.compile(r"^[\w_\-\.\+@]+$")
 
 
 class SignupForm(forms.Form):
@@ -38,7 +38,7 @@ class SignupForm(forms.Form):
     
     def clean_username(self):
         if not alnum_re.search(self.cleaned_data["username"]):
-            raise forms.ValidationError(_("Usernames can only contain letters, numbers and underscores."))
+            raise forms.ValidationError(_("Usernames can only contain letters, numbers andthe following special characters @/./+/-/_"))
         qs = User.objects.filter(username__iexact=self.cleaned_data["username"])
         if not qs.exists():
             return self.cleaned_data["username"]
