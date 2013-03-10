80a094eba8ece2d03ed6a2a7106e0dcd6699cae0
Nathaniel Tucker <me@ntucker.me>
2013-03-09 18:35:30 -0800

    Add south introspection rules for TimeZoneField

--- account/fields.py
+++ account/fields.py
@@ -16,3 +16,21 @@ class TimeZoneField(models.CharField):
         }
         defaults.update(kwargs)
         return super(TimeZoneField, self).__init__(*args, **defaults)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    rules = [
+      (
+        (TimeZoneField,),
+        [],
+        {
+            "max_length": ["max_length", {"default": 100}],
+            "default": ["default", {"default": ""}],
+            "choices": ["choices", {"default": settings.ACCOUNT_TIMEZONES}],
+            "blank": ["blank", {"default": True}],
+        },
+      )
+    ]
+    add_introspection_rules(rules, ["^account\.fields\.TimeZoneField"])
+except ImportError:
+    pass
